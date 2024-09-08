5155f2e977f1d53ea9611e8c81da631a0d2916d5
DESKTOP-TDPQ084\pc <c>
2024-09-08 13:27:03 -0700

    feat: add buttons

--- main.gd
+++ main.gd
@@ -11,12 +11,22 @@ var collected_buildings: Array = []
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	$SpawnTimer.wait_time = spawn_interval
+	$Button.connect("pressed", Callable(self, "_on_Button_pressed"))
+
+func _on_Button_pressed() -> void:
 	$SpawnTimer.start()
+	$Button.hide()  # Optionally hide the button after starting the game
 
 func _on_spawn_timer_timeout() -> void:
-	var building = building_scene.instantiate()
-	add_child(building)
-	building.collected.connect(_on_building_collected)
+	if collected_buildings.size() * building_spacing < screen_size.x:
+		var building = building_scene.instantiate()
+		add_child(building)
+		building.collected.connect(_on_building_collected)
+	else:
+		$SpawnTimer.stop()
+		$MessageLabel.text = "You found yourself in your new city, good job!"
+		$MessageLabel.show()
+		$PlayAgainButton.show()
 
 func _on_building_collected(building: Area2D) -> void:
 	collected_buildings.append(building)
@@ -31,3 +41,11 @@ func arrange_collected_buildings() -> void:
 			screen_size.y - bottom_margin
 		)
 		building.set_collected_position(new_position)
+
+func _on_PlayAgainButton_pressed() -> void:
+	for building in collected_buildings:
+		building.queue_free()
+	collected_buildings.clear()
+	$MessageLabel.hide()
+	$PlayAgainButton.hide()
+	$Button.show()
